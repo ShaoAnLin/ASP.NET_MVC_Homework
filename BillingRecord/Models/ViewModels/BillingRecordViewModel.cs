a710f8b1f3679bde667451b72b8b57dd884bd078
Shawn Lin <shawn.lin@garmin.com>
2018-01-17 14:31:45 +0800

    Add BillingRecordViewModel

---
+++ BillingRecord/Models/ViewModels/BillingRecordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillingRecord.Models.ViewModels
+{
+	public class BillingRecordViewModel
+	{
+		public List<BillingInfoViewModel> RecordList { get; set; }
+	}
+}
