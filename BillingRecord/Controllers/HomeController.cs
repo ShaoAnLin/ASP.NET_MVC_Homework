6ab41004b34710c7287e8e1572bd43708a36cd08
Shawn Lin <shawn.lin@garmin.com>
2018-01-17 14:27:34 +0800

    Set RecordList as a ChildAction

--- BillingRecord/Controllers/HomeController.cs
+++ BillingRecord/Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace BillingRecord.Controllers
 			return View();
 		}
 
+		[ChildActionOnly]
+		public ActionResult RecordList()
+		{
+			return View();
+		}
+
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
