d2286033772ebd6a5bdd4f4ff206a78e7efc010e
Shawn Lin <shawn.lin@garmin.com>
2018-02-01 10:45:45 +0800

    Add display name for the view model

--- BillingRecord/Models/ViewModels/BillingInfoViewModel.cs
+++ BillingRecord/Models/ViewModels/BillingInfoViewModel.cs
@@ -15,15 +15,19 @@ namespace BillingRecord.Models.ViewModels
 			Income = 1
 		}
 
+		[Display(Name = "類別")]
 		public BillingType Type { get; set; }
-		
+
+		[Display(Name = "金額")]
 		[Range(0, int.MaxValue, ErrorMessage="請輸入正整數")]
 		public int Amount { get; set; }
-		
+
+		[Display(Name = "日期")]
 		[Remote("Index", "Validate", ErrorMessage="日期不能大於今天")]
 		public DateTime Date { get; set; }
-		
+
 		[Required]
+		[Display(Name = "備註")]
 		[StringLength(100, ErrorMessage="請勿超過100字")]
 		public string Message { get; set; }
 	}
