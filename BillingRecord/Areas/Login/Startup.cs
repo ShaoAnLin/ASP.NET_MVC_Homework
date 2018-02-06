058cc9d8546bfa728c4a7751772fa3d35edfcb1e
Shawn Lin <shawn.lin@garmin.com>
2018-02-06 18:48:56 +0800

    Add identity related files into "Login" area

---
+++ BillingRecord/Areas/Login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BillingRecord.Startup))]
+namespace BillingRecord
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
