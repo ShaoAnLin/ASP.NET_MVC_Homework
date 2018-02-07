206bece24f9dfe9e5741987e5a945b617203fdf5
Shawn Lin <shawn.lin@garmin.com>
2018-02-07 11:53:56 +0800

    Remove about/contact and use same layout

--- BillingRecord/Controllers/HomeController.cs
+++ BillingRecord/Controllers/HomeController.cs
@@ -45,19 +45,5 @@ namespace BillingRecord.Controllers
 		{	
 			return View(_contentSvc.GetRecords());
 		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
